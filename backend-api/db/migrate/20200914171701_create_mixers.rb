6219f039f4a6789d99b85cca2b1a27b4f6deac24
barrettanderson <barrettanderson1989@gmail.com>
2020-09-14 10:17:48 -0700

    added mixer, join table for ingredients and cocktail

---
+++ backend-api/db/migrate/20200914171701_create_mixers.rb
@@ -0,0 +1,11 @@
+class CreateMixers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :mixers do |t|
+
+      t.integer :cocktail_id
+      t.integer :ingredient_id
+      t.string :measurement
+      t.timestamps
+    end
+  end
+end
