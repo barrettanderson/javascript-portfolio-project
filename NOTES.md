13e21ef59bada25c437894b7d766a4221d568ed0
barrettanderson <barrettanderson1989@gmail.com>
2020-09-14 10:29:45 -0700

    cocktail controller index, create, and params

--- NOTES.md
+++ NOTES.md
@@ -1,1 +1,19 @@
 testing
+
+Join table for the ingredients and the cocktail.
+    This could have the id of the ingredient and the cocktail and then the measurement
+
+Cocktail (has_many ingredients through: :ingredientcocktailjointableextraordinaire)
+    (id)
+    name
+    description/history
+
+Join Table - (belongs_to cocktail, belongs_to ingredient)
+    (id)
+    cocktail_id
+    ingredient_id
+    measurement - string
+
+Ingredients (has_many cocktails through :ingredientcocktailjointableextraordinaire)
+    (id)
+    name
--- backend-api/app/controllers/cocktails_controller.rb
+++ backend-api/app/controllers/cocktails_controller.rb
@@ -1,2 +1,21 @@
 class CocktailsController < ApplicationController
+    
+    def index
+        @cocktails = Cocktail.all
+        render :json @cocktails
+    end
+
+    def create
+        @cocktail = Cocktail.new(cocktail_params)
+        if @cocktail.save
+            render :json @cocktail, status: :created
+        else
+            render :json @cocktail.errors.full_messages, status: :unprocessable_entity
+        end
+    end
+
+    private
+        def cocktail_params
+            params.require(:cocktail).permit(:name, :description)
+        end
 end
