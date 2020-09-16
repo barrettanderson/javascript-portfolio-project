3fd3a79a868b13fe5701bc5c5f5b47680f0dbee4
barrettanderson <barrettanderson1989@gmail.com>
2020-09-16 12:37:00 -0700

    edit and delete button for ingredient

--- frontend/javascripts/models/ingredient.js
+++ frontend/javascripts/models/ingredient.js
@@ -28,5 +28,34 @@ class Ingredient {
         // I want to create a collection select that has all ingredients
         // currently found or create a new one
         const checkbox = document.createElement('checkbox');
+        const span = document.createElement('span')
+        const editButton = document.createElement('button')
+        const deleteButton = document.createElement('button')
+
+        editButton.classList.add('butn')
+        editButton.innerText = 'edit'
+        editButton.id = this.id
+        editButton.addEventListener('click', editIngredient)
+
+        deleteButton.classList.add('btn')
+        deleteButton.innerText = 'delete'
+        deleteButton.id = this.id
+        deleteButton.addEventListener('click', deleteIngredient)
+
+        checkbox.name = this.name
+        checkbox.id = this.id
+        span.innerText = this.name
+        
+        div.appendChild(checkbox)
+        div.appendChild(span)
+
+    }
+
+    editIngredient() {
+
+    }
+
+    deleteIngredient() {
+        
     }
 }
