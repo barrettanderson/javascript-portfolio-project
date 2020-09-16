78a2ff9a1251a56bbb575aa525e6493f46d23713
barrettanderson <barrettanderson1989@gmail.com>
2020-09-16 11:39:47 -0700

    added index and private methods for ingredients controller

--- backend-api/app/controllers/ingredients_controller.rb
+++ backend-api/app/controllers/ingredients_controller.rb
@@ -1,5 +1,22 @@
 class IngredientsController < ApplicationController
+    before_action :set_ingredient, only: [:show, :update, :destroy]
+    
+    def index
+        @ingredients = Ingredient.all
+        render json: @ingredients, :except => [:created_at, :updated_at]
+    end
+
     def create
         # findorcreateby(params:name)
     end
+
+    private
+        def set_ingredient
+            @ingredient = Ingredient.find(params[:id])
+        end
+
+        def ingredient_params
+            params.require(:ingredient).permit(:name)
+        end
+
 end
