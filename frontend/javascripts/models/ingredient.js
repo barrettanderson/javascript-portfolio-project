a6e47e6b564ca43a67abcfb0217739c78528fcac
barrettanderson <barrettanderson1989@gmail.com>
2020-09-16 11:49:57 -0700

    loadIngredients function

--- frontend/javascripts/models/ingredient.js
+++ frontend/javascripts/models/ingredient.js
@@ -3,4 +3,16 @@ class Ingredient {
     this.id = id,
     this.name = name
     }
+
+    function loadIngredients() {
+        fetch(baseUrl + '/ingredients.json')
+            .then(resp => {
+                if (resp.status !== 200) {
+                    throw new Error(resp.statusText)
+                }
+                return resp.json()
+            })
+            .catch(errors => console.log(errors))
+            .then(data => displayIngredients(data))
+    }
 }
